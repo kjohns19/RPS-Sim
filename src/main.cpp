09e3d4b97c77ffcac38eae4704202bf9dcf725f0
Kevin Johnson <krjohnson111@gmail.com>
2020-05-09 08:59:06 -0400

    Print time per update

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <SFML/Graphics.hpp>
 
 #include <atomic>
+#include <chrono>
 #include <iostream>
 #include <mutex>
 #include <thread>
@@ -27,16 +28,20 @@ int main(int argc, char *argv[])
         int iteration = 0;
         while (1)
         {
+            auto start = std::chrono::high_resolution_clock::now();
             for (int i = 0; i < width * height / 50 && !done; i++)
             {
                 std::lock_guard<std::mutex> guard(updateMutex);
                 for (int j = 0; j < 50; j++)
                     grid.updateRandomCell(random);
             }
+            auto end = std::chrono::high_resolution_clock::now();
+            auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
             if (done)
                 break;
             std::cout << iteration << std::endl;
             iteration++;
+            std::cout << (diff.count() / static_cast<double>(width * height)) << std::endl;
         }
     });
 
