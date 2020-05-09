9da2c1745741ef79785dd9fe042821853dd595cd
Kevin Johnson <krjohnson111@gmail.com>
2020-05-09 12:58:01 -0400

    Green -> Yellow

--- src/sim/cell.cpp
+++ src/sim/cell.cpp
@@ -7,7 +7,7 @@ sf::Color Cell::color() const
     static sf::Color s_colors[] = {
         sf::Color::Black,
         sf::Color::Red,
-        sf::Color::Green,
+        sf::Color::Yellow,
         sf::Color::Blue,
     };
     return s_colors[d_value];
